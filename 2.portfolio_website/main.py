07db80e5b953998f37d00d5c6570e7f7e5065d68
neeraj <neerajp4321@gmail.com>
2022-12-26 17:07:26 +0530

    Feat: add flask main and projects data

---
+++ 2.portfolio_website/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from project_data import my_projects
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "28436h45hsgishgjshg89sgy"
+Bootstrap(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", projects=my_projects)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This project folder contains 20 python projects:
 
 1. [Morse code translater](https://github.com/nkp1111/python-projects/tree/main/1.morse_code_translater)
-2. [Portfolio Website]
+2. [Portfolio Website](https://github.com/nkp1111/python-projects/tree/main/2.portfolio_website)
