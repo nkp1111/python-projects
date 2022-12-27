f0fda5f8b5f20be8df8558bb3ec94bfad8a2b355
neeraj <neerajp4321@gmail.com>
2022-12-27 15:19:56 +0530

    Feat: complete project watermark logo

--- 4.watermark_logo/main.py
+++ 4.watermark_logo/main.py
@@ -7,33 +7,25 @@ from tkinter import Tk, Label, Entry, Button, END
 from PIL import Image, ImageDraw, ImageFont
 
 
-# from tkinter.messagebox import showinfo
-
-
-# **********************************************/
-def begin_draw(width, height, image, text):
-    """
-    Draw watermark on image
-    :return:
-    """
-    img_draw = ImageDraw.Draw(image)
-    img_draw.text((50, 50), text, fill=(0, 0, 0), font=ImageFont.truetype("arial.ttf", 35))
-    img_name = image.filename.split("/")[-1]
-    image.save(f"watermarked_images/wm-{img_name}")
-
-
 # **********************************************/
 # add watermark function
 def add_watermark():
     """
-    Add watermark to image
+    Add watermark to image and save it into watermarked_images folder.
     :return:
     """
     img_file = image_input.get()
     watermark = wm_input.get()
     img = Image.open(img_file)
-    width, height = img.size
-    begin_draw(width, height, img, watermark)
+    img_draw = ImageDraw.Draw(img)
+    img_draw.text((50, 50),
+                  text=watermark,
+                  fill=(0, 0, 0),
+                  font=ImageFont.truetype("arial.ttf", 40),
+                  stroke_fill="white",
+                  stroke_width=3)
+    img_name = img.filename.split("/")[-1]
+    img.save(f"watermarked_images/wm-{img_name}")
 
 
 # **********************************************/
@@ -59,7 +51,7 @@ heading.config(bg=NIGHT_BLUE, fg=CREAM, highlightthickness=0, font=HEADING_FONT)
 heading.grid(row=0, column=0, columnspan=2, pady=30)
 
 # input field for image file
-img_label = Label(text="Provide image file path: ")
+img_label = Label(text="Image file path: ")
 img_label.config(bg=NIGHT_BLUE, fg=CREAM, highlightthickness=0, font=("Ariel", 15))
 img_label.grid(row=3, column=0, pady=10)
 
@@ -70,7 +62,7 @@ image_input.config(width=50)
 image_input.grid(row=3, column=1)
 
 # watermark text input field
-wm_text_label = Label(text="Add watermark text: ")
+wm_text_label = Label(text="Watermark text: ")
 wm_text_label.config(bg=NIGHT_BLUE, fg=CREAM, highlightthickness=0, font=("Ariel", 15))
 wm_text_label.grid(row=4, column=0)
 
@@ -81,7 +73,12 @@ wm_input.grid(row=4, column=1)
 
 # submit button
 submit_btn = Button(text="Confirm")
-submit_btn.config(command=add_watermark)
-submit_btn.grid(row=5, column=1, pady=20)
+submit_btn.config(
+    width=30,
+    height=2,
+    command=add_watermark,
+    bg=CREAM,
+)
+submit_btn.grid(row=5, column=0, columnspan=2, pady=30)
 
 window.mainloop()
