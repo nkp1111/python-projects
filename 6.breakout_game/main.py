56b80cd170fddc645fcab65380f72ca567b2ebc3
neeraj <neerajp4321@gmail.com>
2022-12-28 11:21:35 +0530

    Feat: add movement to player

--- 6.breakout_game/main.py
+++ 6.breakout_game/main.py
@@ -8,6 +8,16 @@ from player import Player
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 300
 
+
+# ------------------------------------------
+# funtions
+
+
+
+
+
+# ------------------------------------------
+# turtle components
 screen = Screen()
 screen.title("Breakout Game")
 screen.bgcolor("#000")
@@ -16,5 +26,12 @@ screen.tracer(0)
 
 player = Player()
 screen.update()
+screen.listen()
+screen.onkeypress(player.move_left, "a")
+screen.onkeypress(player.move_right, "d")
+
+while True:
+    screen.update()
+
 
 screen.exitonclick()
--- 6.breakout_game/player.py
+++ 6.breakout_game/player.py
@@ -10,4 +10,13 @@ class Player(Turtle):
         self.color("blue")
         self.shapesize(1, 5, 1)
         self.goto(0, -250)
+        self.speed = "fastest"
+
+    def move_left(self):
+        self.setheading(180)
+        self.fd(10)
+
+    def move_right(self):
+        self.setheading(0)
+        self.fd(10)
 
