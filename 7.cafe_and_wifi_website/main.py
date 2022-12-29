1063faef4e1b5fe2dc27219f88406470e44d6df3
nkp1111 <neerajp4321@gmail.com>
2022-12-29 10:35:25 +0530

    Feat: add database, template files

--- 7.cafe_and_wifi_website/main.py
+++ 7.cafe_and_wifi_website/main.py
@@ -13,9 +13,49 @@ Bootstrap(app)
 db = SQLAlchemy(app)
 
 
+# ------------------------------------
+# database
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(400), nullable=False)
+    img_url = db.Column(db.String(400), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(100), nullable=False)
+    coffee_price = db.Column(db.String(100), nullable=False)
+
+# # create database
+# with app.app_context():
+#     db.create_all()
+# --------------------------------------
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    """
+    Show all Cafes
+    :return:
+    """
+    with app.app_context():
+        all_cafes = db.session.query(Cafe).all()
+
+    return render_template("index.html", cafes=all_cafes)
+
+
+@app.route("/cafe/<int:cafe_id>")
+def get_cafe(cafe_id):
+    """
+    Get a cafe detail into of particular cafe with id
+    :param cafe_id:
+    :return:
+    """
+    with app.app_context():
+        cafe = Cafe.query.get(cafe_id)
+    return render_template("cafe_detail.html", cafe=cafe)
 
 
 if __name__ == "__main__":
