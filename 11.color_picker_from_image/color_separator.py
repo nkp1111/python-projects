37dd91c3a4f1a708f76c9e3796ad7151bbb0de63
nkp1111 <neerajp4321@gmail.com>
2022-12-30 20:30:16 +0530

    Feat: add template and refactor color_separator

--- 11.color_picker_from_image/color_separator.py
+++ 11.color_picker_from_image/color_separator.py
@@ -1,20 +1,19 @@
 from PIL import Image
 import numpy as np
 
-top_colors = []
-top_colors_num = 10
 
-img = Image.open("images/cat-wall.jpg")
-colors = np.array(img)
-print(colors.shape)
-colors = colors.reshape((749 * 1200, 3))
-total_colors = colors.size
+def get_top_colors(image="images/cat-wall.jpg", number_of_colors=10):
+    top_colors = []
+    img = Image.open(image)
+    colors = np.array(img)
+    print(colors.shape)
+    colors = colors.reshape((749 * 1200, 3))
+    total_colors = colors.size
+    values, counts = np.unique(colors, return_counts=True, axis=0)
 
-values, counts = np.unique(colors, return_counts=True, axis=0)
+    for i in range(number_of_colors):
+        val = np.argmax(counts)
+        top_colors.append((counts[val] / total_colors, values[val]))
+        counts = np.delete(counts,val)
 
-for i in range(top_colors_num):
-    val = np.argmax(counts)
-    top_colors.append((counts[val] / total_colors, values[val]))
-    counts = np.delete(counts,val)
-
-print(top_colors)
+    return top_colors
