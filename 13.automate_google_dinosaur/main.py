31daa7faac118241b457098fd555b7a709a61ccc
nkp1111 <neerajp4321@gmail.com>
2023-01-02 15:00:57 +0530

    Feat: get the dino start

--- 13.automate_google_dinosaur/main.py
+++ 13.automate_google_dinosaur/main.py
@@ -2,12 +2,18 @@
 Automatic Google Dinosaur game
 """
 from selenium.webdriver import Chrome
+import pyautogui
 
 driver = Chrome()
 DINO_GAME_URL = "https://elgoog.im/t-rex/?bot"
 driver.get(DINO_GAME_URL)
-
+pyautogui.PAUSE = 1
+pyautogui.keyDown("space ")
 while True:
+    print(pyautogui.position())
+    print(pyautogui.size())
+    pyautogui.PAUSE = 1
+    pyautogui.keyDown("up")
     pass
 
 
