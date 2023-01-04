da451e76db3d5eea1d0524dc590b95b190ce1103
nkp1111 <neerajp4321@gmail.com>
2023-01-04 11:25:49 +0530

    Feat: convert pdf to audio file

--- 10.pdf_to_audiobook/main.py
+++ 10.pdf_to_audiobook/main.py
@@ -2,6 +2,7 @@
 PDF to audiobook
 """
 import pyttsx3
+import PyPDF2
 
 engine = pyttsx3.init()
 
@@ -21,10 +22,19 @@ male_voice = voices[0].id
 female_voice = voices[1].id
 engine.setProperty("voice", female_voice)
 
-with open("text_files/test.txt") as file:
-    lines = file.read()
-    # engine.say(lines)
-    filename = input("Type a filename for this audio file: ")
-    engine.save_to_file(lines, f"audio_files/{filename}.mp3")
+# with open("text_files/test.txt") as file:
+#     lines = file.read()
+#     # engine.say(lines)
+#     filename = input("Type a filename for this audio file: ")
+#     engine.save_to_file(lines, f"audio_files/{filename}.mp3")
 
+text_content = ""
+with open("text_files/wings_of_fire.pdf", mode="rb") as pdf_file:
+    pdf_book = PyPDF2.PdfReader(pdf_file)
+    pages = len(pdf_book.pages)
+    for page_num in range(pages):
+        page = pdf_book.pages[page_num]
+        text_content += page.extract_text()
+
+engine.save_to_file(text_content, "audio_files/wings_of_fire.mp3")
 engine.runAndWait()
