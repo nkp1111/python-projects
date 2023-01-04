e26a2263cfbee34a0513f5bf0c6d6a5151e8de29
nkp1111 <neerajp4321@gmail.com>
2023-01-04 11:56:01 +0530

    Feat: separate speech conversion and flask app file

--- 10.pdf_to_audiobook/main.py
+++ 10.pdf_to_audiobook/main.py
@@ -1,40 +1,22 @@
 """
 PDF to audiobook
 """
-import pyttsx3
-import PyPDF2
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-engine = pyttsx3.init()
 
-# speech rate or speed of speech delivery defaults 200
-print(engine.getProperty("rate"))
-speech_rate_or_speed = 150
-engine.setProperty("rate", speech_rate_or_speed)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "KSLlsgj9459835yiu34j4k53kj"
+Bootstrap(app)
 
-# volume (0, 1) defaults 1
-print(engine.getProperty("volume"))
-speech_volume = 1.2
-engine.setProperty("volume", speech_volume)
 
-# voices male or female
-voices = engine.getProperty("voices")
-male_voice = voices[0].id
-female_voice = voices[1].id
-engine.setProperty("voice", female_voice)
+# form
 
-# with open("text_files/test.txt") as file:
-#     lines = file.read()
-#     # engine.say(lines)
-#     filename = input("Type a filename for this audio file: ")
-#     engine.save_to_file(lines, f"audio_files/{filename}.mp3")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-text_content = ""
-with open("text_files/wings_of_fire.pdf", mode="rb") as pdf_file:
-    pdf_book = PyPDF2.PdfReader(pdf_file)
-    pages = len(pdf_book.pages)
-    for page_num in range(pages):
-        page = pdf_book.pages[page_num]
-        text_content += page.extract_text()
 
-engine.save_to_file(text_content, "audio_files/wings_of_fire.mp3")
-engine.runAndWait()
+if __name__ == "__main__":
+    app.run(debug=True)
+
